c2b85ca6cf069374c48e0d48458d2d7e6fe333bb
Filip Tammergard <filip@tammergard.se>
2019-11-16 22:50:02 +0100

    nedräknare och önskelista

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -2,33 +2,37 @@
   <div id="welcome-component">
     <h1>Välkommen!</h1>
     <p>Hej och välkommen till vår bröllopshemsida! Här har vi samlat information som kan vara bra för dig som bröllopsgäst att få ta del av.</p>
-    <h2>Nedräkning</h2>
-    <div class>
-      <div class="card">
-        <div class="card-header">Dagar</div>
-        <div class="card-body">
-          <p>{{ daysToWedding }}</p>
-        </div>
+    <div class="d-flex flex-row justify-content-around countdown">
+      <div>
+        <p>
+          <span>{{ daysToWedding }}</span>
+          <br />
+          <span>dagar</span>
+        </p>
       </div>
-      <div class="card">
-        <div class="card-header">Timmar</div>
-        <div class="card-body">
-          <p>{{ hoursToWedding }}</p>
-        </div>
+      <div>
+        <p>
+          <span>{{ hoursToWedding }}</span>
+          <br />
+          <span>timmar</span>
+        </p>
       </div>
-      <div class="card">
-        <div class="card-header">Minuter</div>
-        <div class="card-body">
-          <p>{{ minutesToWedding }}</p>
-        </div>
+      <div>
+        <p>
+          <span>{{ minutesToWedding }}</span>
+          <br />
+          <span>minuter</span>
+        </p>
       </div>
-      <div class="card">
-        <div class="card-header">Sekunder</div>
-        <div class="card-body">
-          <p>{{ secondsToWedding }}</p>
-        </div>
+      <div>
+        <p>
+          <span>{{ secondsToWedding }}</span>
+          <br />
+          <span>sekunder</span>
+        </p>
       </div>
     </div>
+    <h2 style="text-align: center;">... kvar till Alice &amp; Jakobs vigsel</h2>
   </div>
 </template>
 
@@ -45,7 +49,7 @@ export default {
     };
   },
   created() {
-    let countDownDate = new Date("Aug 1, 2020 13:00:00").getTime();
+    let countDownDate = new Date("Aug 1, 2020 14:30:00").getTime();
     this.countDownDate(countDownDate);
     setInterval(() => {
       this.countDownDate(countDownDate);
@@ -53,13 +57,8 @@ export default {
   },
   methods: {
     countDownDate(countDownDate) {
-      // Get today's date and time
       let now = new Date().getTime();
-
-      // Find the distance between now and the count down date
       let distance = countDownDate - now;
-
-      // Time calculations for days, hours, minutes and seconds
       this.daysToWedding = Math.floor(distance / (1000 * 60 * 60 * 24));
       this.hoursToWedding = Math.floor(
         (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
--- src/components/Wishlist.vue
+++ src/components/Wishlist.vue
@@ -2,6 +2,19 @@
   <div id="wishlist-component">
     <h1>Önskelista</h1>
     <p>Den finaste presenten ni kan ge är att få är att vi får dela vår bröllopsdag med er. Vill ni ändå gärna ge något extra så är här tips på saker som vi önskar oss!</p>
+    <div class="card" style="width: 18rem;">
+      <div>
+        <img
+          src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTJqRON3Ff7Wx0PeuvtxPvDSs5cHCplVFx4WfWpQrbmhxaiS0a055CO1St3qhrd4KqHiU5mPVhmvpPaM7ulUq4bCVj0zJaOBd-UZqSlG44bkCoSnr_XvdE_zw&usqp=CAc"
+          class="card-img-top"
+        />
+      </div>
+      <div class="card-body">
+        <b>Tallrik: Ikea Modell</b>
+        <p class="card-text"></p>
+        <button class="btn btn-secondary btn-block" type="submit">Visa</button>
+      </div>
+    </div>
   </div>
 </template>
 
