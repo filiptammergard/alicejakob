db3eec446c224a732bc7ee224964591cc59c3fec
Filip Tammergard <filip@tammergard.se>
2019-11-17 11:22:11 +0100

    countfown

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="welcome-component">
-    <h1>Välkommen!</h1>
-    <p>Hej och välkommen till vår bröllopshemsida! Här har vi samlat information som kan vara bra för dig som bröllopsgäst att få ta del av.</p>
     <br />
   </div>
 </template>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQeP9sAuemgKVDcbIOIY-zyJsri2GadOo",
+  authDomain: "onskelista-alicejakob.firebaseapp.com",
+  databaseURL: "https://onskelista-alicejakob.firebaseio.com",
+  projectId: "onskelista-alicejakob",
+  storageBucket: "onskelista-alicejakob.appspot.com",
+  messagingSenderId: "900988406742",
+  appId: "1:900988406742:web:da466c9e2dea8a9ccd3277",
+  measurementId: "G-XGJKVHRPHD"
+};
+
+export default firebaseConfig;
