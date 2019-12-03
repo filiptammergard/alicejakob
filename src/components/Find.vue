3bba5f42c791f9d0064b5131d6bf9012f253d3eb
Filip Tammergard <filip@tammergard.se>
2019-12-03 19:09:36 +0100

    spacing

--- src/components/Find.vue
+++ src/components/Find.vue
@@ -30,7 +30,7 @@
           <div class="col-md-6 align-self-center">
             <h2>Marma Gård</h2>
             <p>Tid: Återkommer</p>
-            <p>Adress: Danmarks kyrkby, 755 98 Uppsala</p>
+            <p>Adress: Marma 113, 741 93 Knivsta</p>
             <p>Vi ser fram emot att samla våra familjer och vänner efter vigseln, ni betyder mycket för oss! Bröllopsfesten kommer hållas i Marmasalen på Marma Gård strax utanför Knivsta, cirka 10 minuters bilfärd vidare från Danmarks Kyrka.</p>
           </div>
         </div>
