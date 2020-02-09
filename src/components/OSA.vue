b6a8579326aff7b705c6f4d02abaafd57ce4a61b
Filip Tammergard <filip@tammergard.se>
2020-02-09 18:34:45 +0100

    osa datum

--- src/components/OSA.vue
+++ src/components/OSA.vue
@@ -3,10 +3,10 @@
     <section class="white d-flex align-items-center">
       <div class="container">
         <div class="align-self-center">
-          <h1>O.S.A</h1>
+          <h1>O.S.A.</h1>
           <p>
             I det här formuläret kan du som har fått en inbjudan i brevlådan meddela om du kan komma eller inte på bröllopsfesten. Det går också bra att anmäla dig eller meddela att du inte kan komma genom att höra av dig till värdarna på annat sätt, se kontaktuppgifter i fliken
-            <router-link :to="{ name: 'contact' }">Kontakt</router-link>.
+            <router-link :to="{ name: 'contact' }">Kontakt</router-link>. O.S.A. senast 1 juni.
           </p>
         </div>
       </div>
