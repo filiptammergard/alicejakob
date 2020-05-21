1d3a82a2920b764fe8fe8f56b30d804b17150fa1
Filip Tammergard <filip@tammergard.se>
2020-05-21 09:27:15 +0200

    visa läs mer i stället för hela meddelandet i popupen

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -94,19 +94,10 @@
           <p>
             Hej kära familj och vänner! Om mindre tre månader ska vi äntligen få säga JA till
             varandra! Vi vill dela med er om hur våra tankar går med avseende på den rådande
-            situationen med Covid-19. När vi skickade ut inbjudningarna för tre månader sen visste
-            vi knappt vad corona var. Nu är det något som påverkar allas vår vardag. Det kommer med
-            största sannolikhet också påverka hur vi kommer kunna utforma vårt bröllop den 1
-            augusti. I dagsläget är det exempelvis inte tillåtet eller lämpligt för oss att ha en
-            bröllopsfest i den utformningen vi hade planerat.<br />
-            Samtidigt är det nästan tre månader kvar och vi hoppas fortfarande att vi kommer kunna
-            ha bröllopsfesten som planerat med alla er där. Om det inte blir fallet kommer vi
-            utforma vårt bröllop på ett annat sätt och göra det så bra vi kan utifrån
-            förutsättningarna och vad som känns rätt för oss. Oavsett kommer vi ta ett beslut och
-            informera er senast i början av juni, när vi vet mer om läget och myndigheter har kommit
-            ut med mer information och riktlinjer. Vi skjuter därmed också upp OSA datum till den
-            15e juni. Ta hand om er! <br /><br />
-            Hälsningar, Alice och Jakob
+            situationen med Covid-19. Meddelandet finns
+            <a href="#corona-component" @click="hideCoronaMessagePopUp()"
+              >längst ner på den här sidan</a
+            >. Hälsningar, Alice och Jakob
           </p>
           <div>
             <button class="btn btn-secondary" @click="hideCoronaMessagePopUp()">Avbryt</button>
