24b4ba9f7569b473978105aff9bae3cef4f48bf1
Filip Tammergard <filip@tammergard.se>
2020-05-21 09:20:48 +0200

    hide scroller on popup show

--- src/components/Corona.vue
+++ src/components/Corona.vue
@@ -10,13 +10,14 @@
           knappt vad corona var. Nu är det något som påverkar allas vår vardag. Det kommer med
           största sannolikhet också påverka hur vi kommer kunna utforma vårt bröllop den 1 augusti.
           I dagsläget är det exempelvis inte tillåtet eller lämpligt för oss att ha en bröllopsfest
-          i den utformningen vi hade planerat. Samtidigt är det nästan tre månader kvar och vi
-          hoppas fortfarande att vi kommer kunna ha bröllopsfesten som planerat med alla er där. Om
-          det inte blir fallet kommer vi utforma vårt bröllop på ett annat sätt och göra det så bra
-          vi kan utifrån förutsättningarna och vad som känns rätt för oss. Oavsett kommer vi ta ett
-          beslut och informera er senast i början av juni, när vi vet mer om läget och myndigheter
-          har kommit ut med mer information och riktlinjer. Vi skjuter därmed också upp OSA datum
-          till den 15e juni. Ta hand om er! <br /><br />
+          i den utformningen vi hade planerat.<br />
+          Samtidigt är det nästan tre månader kvar och vi hoppas fortfarande att vi kommer kunna ha
+          bröllopsfesten som planerat med alla er där. Om det inte blir fallet kommer vi utforma
+          vårt bröllop på ett annat sätt och göra det så bra vi kan utifrån förutsättningarna och
+          vad som känns rätt för oss. Oavsett kommer vi ta ett beslut och informera er senast i
+          början av juni, när vi vet mer om läget och myndigheter har kommit ut med mer information
+          och riktlinjer. Vi skjuter därmed också upp OSA datum till den 15e juni. Ta hand om er!
+          <br /><br />
           Hälsningar, Alice och Jakob
         </p>
       </div>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -21,7 +21,7 @@
           </div>
         </div>
       </div>
-      <a href="#info" class="scroll-down"></a>
+      <a href="#info" class="scroll-down" :class="{ hide: coronaMessageVisible }"></a>
     </section>
     <section id="info" class="dark row contains-image">
       <div class="container col-md-6 d-flex align-items-center">
@@ -98,13 +98,14 @@
             vi knappt vad corona var. Nu är det något som påverkar allas vår vardag. Det kommer med
             största sannolikhet också påverka hur vi kommer kunna utforma vårt bröllop den 1
             augusti. I dagsläget är det exempelvis inte tillåtet eller lämpligt för oss att ha en
-            bröllopsfest i den utformningen vi hade planerat. Samtidigt är det nästan tre månader
-            kvar och vi hoppas fortfarande att vi kommer kunna ha bröllopsfesten som planerat med
-            alla er där. Om det inte blir fallet kommer vi utforma vårt bröllop på ett annat sätt
-            och göra det så bra vi kan utifrån förutsättningarna och vad som känns rätt för oss.
-            Oavsett kommer vi ta ett beslut och informera er senast i början av juni, när vi vet mer
-            om läget och myndigheter har kommit ut med mer information och riktlinjer. Vi skjuter
-            därmed också upp OSA datum till den 15e juni. Ta hand om er! <br /><br />
+            bröllopsfest i den utformningen vi hade planerat.<br />
+            Samtidigt är det nästan tre månader kvar och vi hoppas fortfarande att vi kommer kunna
+            ha bröllopsfesten som planerat med alla er där. Om det inte blir fallet kommer vi
+            utforma vårt bröllop på ett annat sätt och göra det så bra vi kan utifrån
+            förutsättningarna och vad som känns rätt för oss. Oavsett kommer vi ta ett beslut och
+            informera er senast i början av juni, när vi vet mer om läget och myndigheter har kommit
+            ut med mer information och riktlinjer. Vi skjuter därmed också upp OSA datum till den
+            15e juni. Ta hand om er! <br /><br />
             Hälsningar, Alice och Jakob
           </p>
           <div>
