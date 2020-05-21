9b2eded0bdd8b81938780f36cc896657a86aedc7
Filip Tammergard <filip@tammergard.se>
2020-05-21 11:30:24 +0200

    small fix again

--- src/components/Corona.vue
+++ src/components/Corona.vue
@@ -6,12 +6,11 @@
         <p>
           Hej kära familj och vänner! Snart ska vi äntligen få säga JA till varandra! Vi vill dela
           med er om hur våra tankar går med avseende på den rådande situationen med Covid-19. När vi
-          skickade ut inbjudningarna för tre månader sen visste vi knappt vad corona var. Nu är det
-          något som påverkar allas vår vardag. Det kommer med största sannolikhet också påverka hur
-          vi kommer kunna utforma vårt bröllop den 1 augusti. I dagsläget är det exempelvis inte
-          tillåtet eller lämpligt för oss att ha en bröllopsfest i den utformningen vi hade
-          planerat.<br />
-          Samtidigt är det nästan tre månader kvar och vi hoppas fortfarande att vi kommer kunna ha
+          skickade ut inbjudningarna visste vi knappt vad corona var. Nu är det något som påverkar
+          allas vår vardag. Det kommer med största sannolikhet också påverka hur vi kommer kunna
+          utforma vårt bröllop den 1 augusti. I dagsläget är det exempelvis inte tillåtet eller
+          lämpligt för oss att ha en bröllopsfest i den utformningen vi hade planerat.<br />
+          Samtidigt är det ett tag kvar och vi hoppas fortfarande att vi kommer kunna ha
           bröllopsfesten som planerat med alla er där. Om det inte blir fallet kommer vi utforma
           vårt bröllop på ett annat sätt och göra det så bra vi kan utifrån förutsättningarna och
           vad som känns rätt för oss. Oavsett kommer vi ta ett beslut och informera er senast i
